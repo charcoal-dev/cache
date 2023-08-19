cf27eccab1dc3d5c35057db509d5c3f1eeb1d0f8
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-08-19 21:10:06 +0400

    reference exceptions

--- src/Exception/CachedEntityError.php
+++ src/Exception/CachedEntityError.php
@@ -25,4 +25,8 @@ enum CachedEntityError: int
     case UNSERIALIZE_FAIL = 0x12c;
     case CHECKSUM_NOT_STORED = 0x190;
     case BAD_CHECKSUM = 0x1f4;
+    case REF_KEY_LENGTH = 0x258;
+    case REF_DECODE_ERROR = 0x2bc;
+    case REF_NOT_OBJECT = 0x320;
+    case REF_BAD_CHECKSUM = 0x384;
 }
--- src/Exception/CachedEntityException.php
+++ src/Exception/CachedEntityException.php
@@ -14,12 +14,17 @@ declare(strict_types=1);
 
 namespace Charcoal\Cache\Exception;
 
+use Charcoal\Buffers\Frames\Bytes20;
+
 /**
  * Class CachedEntityException
  * @package Charcoal\Cache\Exception
  */
 class CachedEntityException extends CacheException
 {
+    public ?Bytes20 $checksum1 = null;
+    public ?Bytes20 $checksum2 = null;
+
     /**
      * @param \Charcoal\Cache\Exception\CachedEntityError $error
      * @param string $msg
@@ -32,5 +37,25 @@ class CachedEntityException extends CacheException
     )
     {
         parent::__construct($msg, $this->error->value, $previous);
+    }
+
+    /**
+     * @param \Charcoal\Cache\Exception\CachedEntityError $flag
+     * @param \Charcoal\Buffers\Frames\Bytes20|null $checksum1
+     * @param \Charcoal\Buffers\Frames\Bytes20|null $checksum2
+     * @param \Throwable|null $previous
+     * @return static
+     */
+    public static function ChecksumError(
+        CachedEntityError $flag,
+        ?Bytes20          $checksum1 = null,
+        ?Bytes20          $checksum2 = null,
+        ?\Throwable       $previous = null,
+    ): static
+    {
+        $ex = new static($flag, "", $previous);
+        $ex->checksum1 = $checksum1;
+        $ex->checksum2 = $checksum2;
+        return $ex;
     }
 }
