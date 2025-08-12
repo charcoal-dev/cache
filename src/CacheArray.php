ccb7d09f8c4eb92d7893ab6632cc52fc115ea6f8
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-12 08:59:25 +0500

    refactor CacheArray to use InstancedObjectsRegistry, update type hints, and streamline server operations

--- src/CacheArray.php
+++ src/CacheArray.php
@@ -8,7 +8,8 @@ declare(strict_types=1);
 
 namespace Charcoal\Cache;
 
-
+use Charcoal\Base\Concerns\InstancedObjectsRegistry;
+use Charcoal\Base\Concerns\RegistryKeysLowercaseTrimmed;
 use Charcoal\Buffers\Frames\Bytes20;
 use Charcoal\Cache\CacheArray\BulkCacheOp;
 use Charcoal\Cache\Exception\CacheArrayException;
@@ -18,68 +19,56 @@ use Charcoal\Cache\Exception\CacheException;
 /**
  * Class CacheArray
  * @package Charcoal\Cache
+ * @template-implements InstancedObjectsRegistry<array, CacheClient>
+ * @property array<string,CacheClient> $instances
  */
 class CacheArray implements \IteratorAggregate, CacheApiInterface
 {
-    protected array $stores = [];
+    protected ?CacheClient $primary = null;
     protected int $count = 0;
-    protected ?Cache $primary = null;
 
-    /**
-     * @param \Charcoal\Cache\Cache $cache
-     * @return $this
-     */
-    public function addServer(Cache $cache): static
+    use InstancedObjectsRegistry;
+    use RegistryKeysLowercaseTrimmed;
+
+    public function addServer(CacheClient $cache): static
     {
-        if (!isset($this->stores[$cache->storageDriver->metaUniqueId()])) {
-            $this->stores[$cache->storageDriver->metaUniqueId()] = $cache;
+        if (!isset($this->instances[$cache->storageDriver->metaUniqueId()])) {
+            $this->instances[$cache->storageDriver->metaUniqueId()] = $cache;
             $this->count++;
         }
 
         return $this;
     }
 
-    /**
-     * @param string $key
-     * @return \Charcoal\Cache\Cache|null
-     */
-    public function getServer(string $key): ?Cache
+    public function getServer(string $key): ?CacheClient
     {
-        return array_key_exists($key, $this->stores) ? $this->stores[$key] : null;
+        return array_key_exists($key, $this->instances) ? $this->instances[$key] : null;
     }
 
-    /**
-     * @return int
-     */
     public function count(): int
     {
         return $this->count;
     }
 
-    /**
-     * @return \ArrayIterator
-     */
     public function getIterator(): \ArrayIterator
     {
-        return new \ArrayIterator($this->stores);
+        return new \ArrayIterator($this->instances);
     }
 
     /**
-     * @return \Charcoal\Cache\Cache
      * @throws \Charcoal\Cache\Exception\CacheArrayException
      */
-    protected function getPrimary(): Cache
+    protected function getPrimary(): CacheClient
     {
         if ($this->primary) {
             return $this->primary;
         }
 
-        if (!$this->stores) {
+        if (!$this->instances) {
             throw new CacheArrayException('No servers added to CacheArray');
         }
 
-        /** @var \Charcoal\Cache\Cache $store */
-        foreach ($this->stores as $store) {
+        foreach ($this->instances as $store) {
             if (!$store->isConnected()) {
                 try {
                     $store->connect();
@@ -97,9 +86,7 @@ class CacheArray implements \IteratorAggregate, CacheApiInterface
     }
 
     /**
-     * @param \Closure $callback
-     * @return mixed
-     * @throws \Charcoal\Cache\Exception\CacheException
+     * @throws CacheArrayException
      */
     protected function primaryServerOp(\Closure $callback): mixed
     {
@@ -118,8 +105,7 @@ class CacheArray implements \IteratorAggregate, CacheApiInterface
     protected function allServersOp(\Closure $callback): BulkCacheOp
     {
         $result = new BulkCacheOp();
-        /** @var \Charcoal\Cache\Cache $store */
-        foreach ($this->stores as $store) {
+        foreach ($this->instances as $store) {
             $result->total++;
 
             try {
@@ -135,41 +121,23 @@ class CacheArray implements \IteratorAggregate, CacheApiInterface
     }
 
     /**
-     * @param string $key
-     * @param mixed $value
-     * @param int|null $ttl
-     * @param bool|null $createChecksum
-     * @return bool|\Charcoal\Buffers\Frames\Bytes20
-     * @throws \Charcoal\Cache\Exception\CacheException
+     * @throws CacheArrayException
      */
     public function set(string $key, mixed $value, ?int $ttl = null, ?bool $createChecksum = null): bool|Bytes20
     {
-        return $this->primaryServerOp(function (Cache $primary) use ($key, $value, $ttl, $createChecksum) {
+        return $this->primaryServerOp(function (CacheClient $primary) use ($key, $value, $ttl, $createChecksum) {
             return $primary->set($key, $value, $ttl, $createChecksum);
         });
     }
 
-    /**
-     * @param string $key
-     * @param mixed $value
-     * @param int|null $ttl
-     * @param bool|null $createChecksum
-     * @return \Charcoal\Cache\CacheArray\BulkCacheOp
-     */
     public function setToAll(string $key, mixed $value, ?int $ttl = null, ?bool $createChecksum = null): BulkCacheOp
     {
-        return $this->allServersOp(function (Cache $store) use ($key, $value, $ttl, $createChecksum) {
+        return $this->allServersOp(function (CacheClient $store) use ($key, $value, $ttl, $createChecksum) {
             return $store->set($key, $value, $ttl, $createChecksum);
         });
     }
 
     /**
-     * @param string $key
-     * @param bool $returnCachedEntity
-     * @param bool $returnReferenceKeyObject
-     * @param bool $expectInteger
-     * @param bool|null $verifyChecksum
-     * @return int|string|array|object|bool|null
      * @throws \Charcoal\Cache\Exception\CacheException
      */
     public function get(
@@ -180,19 +148,18 @@ class CacheArray implements \IteratorAggregate, CacheApiInterface
         ?bool  $verifyChecksum = null
     ): int|string|null|array|object|bool
     {
-        return $this->primaryServerOp(function (Cache $primary) use ($key, $returnCachedEntity, $returnReferenceKeyObject, $expectInteger, $verifyChecksum) {
-            return $primary->get($key, $returnCachedEntity, $returnReferenceKeyObject, $expectInteger, $verifyChecksum);
+        return $this->primaryServerOp(function (CacheClient $primary) use (
+            $key,
+            $returnCachedEntity,
+            $returnReferenceKeyObject,
+            $expectInteger,
+            $verifyChecksum
+        ) {
+            return $primary->get($key, $returnCachedEntity, $returnReferenceKeyObject,
+                $expectInteger, $verifyChecksum);
         });
     }
 
-    /**
-     * @param string $key
-     * @param bool $returnCachedEntity
-     * @param bool $returnReferenceKeyObject
-     * @param bool $expectInteger
-     * @param bool|null $verifyChecksum
-     * @return \Charcoal\Cache\CacheArray\BulkCacheOp
-     */
     public function getFromAll(
         string $key,
         bool   $returnCachedEntity = false,
@@ -201,53 +168,48 @@ class CacheArray implements \IteratorAggregate, CacheApiInterface
         ?bool  $verifyChecksum = null
     ): BulkCacheOp
     {
-        return $this->allServersOp(function (Cache $store) use ($key, $returnCachedEntity, $returnReferenceKeyObject, $expectInteger, $verifyChecksum) {
-            return $store->get($key, $returnCachedEntity, $returnReferenceKeyObject, $expectInteger, $verifyChecksum);
+        return $this->allServersOp(function (CacheClient $store) use (
+            $key,
+            $returnCachedEntity,
+            $returnReferenceKeyObject,
+            $expectInteger,
+            $verifyChecksum
+        ) {
+            return $store->get($key, $returnCachedEntity, $returnReferenceKeyObject,
+                $expectInteger, $verifyChecksum);
         });
     }
 
     /**
-     * @param string $key
-     * @return bool
      * @throws \Charcoal\Cache\Exception\CacheException
      */
     public function has(string $key): bool
     {
-        return $this->primaryServerOp(function (Cache $primary) use ($key) {
+        return $this->primaryServerOp(function (CacheClient $primary) use ($key) {
             return $primary->has($key);
         });
     }
 
-    /**
-     * @param string $key
-     * @return \Charcoal\Cache\CacheArray\BulkCacheOp
-     */
     public function allHave(string $key): BulkCacheOp
     {
-        return $this->allServersOp(function (Cache $store) use ($key) {
+        return $this->allServersOp(function (CacheClient $store) use ($key) {
             return $store->has($key);
         });
     }
 
     /**
-     * @param string $key
-     * @return bool
      * @throws \Charcoal\Cache\Exception\CacheException
      */
     public function delete(string $key): bool
     {
-        return $this->primaryServerOp(function (Cache $primary) use ($key) {
+        return $this->primaryServerOp(function (CacheClient $primary) use ($key) {
             return $primary->delete($key);
         });
     }
 
-    /**
-     * @param string $key
-     * @return \Charcoal\Cache\CacheArray\BulkCacheOp
-     */
     public function deleteFromAll(string $key): BulkCacheOp
     {
-        return $this->allServersOp(function (Cache $store) use ($key) {
+        return $this->allServersOp(function (CacheClient $store) use ($key) {
             return $store->delete($key);
         });
     }
